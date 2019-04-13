dade6cfa37f12dc88701e75f8ab3d3df5c4ba1e2
unknown <sammitra47@gmail.com>
2019-04-13 23:41:20 +0300

    add compression to express

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
 const path = require("path");
+var compression = require("compression");
+
 app.use(express.static(path.join(__dirname)));
+app.use(compression());
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
